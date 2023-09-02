13bcc046327512b02c1db389411623353b79b0e9
Tyler Mills <tyler.mills@platform.sh>
2023-09-02 12:01:12 -0400

    Replace alert with tooltip on share button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../assets/logo/upsun_horizontal.svg';
-import ShareButton from './Share';
+import ShareButton from './ShareButton';
 
 function Header() {
   return (
