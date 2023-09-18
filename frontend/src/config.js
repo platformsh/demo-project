9a3714d5fb243080162e247d51416fe07f0135b2
chadcarlson <chad.carlson@platform.sh>
2023-09-18 09:01:02 -0400

    hardcode for now.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 /** The .environment file exports the relevant environment variable for you. */
-const BASE_PATH = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
-
+// const BASE_PATH = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
+const BASE_PATH = "http://api.internal"
 export const API_BASE_PATH = `/api/v1`
 export const API_BASE_URL = `${BASE_PATH}${API_BASE_PATH}`;
