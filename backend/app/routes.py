c35c8cad792d483710f27f3563bd4847c27dd490
Tyler Mills <tyler.mills@platform.sh>
2023-11-03 17:06:35 -0400

    Lint routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,13 @@
-"""This module provides the routes for the application."""
+"""
+This module sets up and defines the routes for the application's API endpoints.
+It establishes routes for accessing the environment settings and home greeting.
+Utility functions to determine session storage type and platform environment are also provided.
+"""
 
 import os
-from flask import Blueprint, jsonify
 import base64
 import json
+from flask import Blueprint, jsonify
 
 bp = Blueprint('routes', __name__)
 
@@ -11,29 +15,39 @@ API_PREFIX = '/api/v1'
 
 @bp.route(f'{API_PREFIX}/environment')
 def environment():
-    return jsonify(type=getPlatformEnvironment(), session_storage=getSessionStorageType())
+    """Return the platform environment and session storage type as JSON."""
+    return jsonify(type=get_platform_environment(), session_storage=get_session_storage_type())
 
 @bp.route('/api/')
 def home():
+    """Return a greeting message from the Python backend."""
     return "Hello from the Python backend!"
 
-def getSessionStorageType():
+def get_session_storage_type():
+    """
+    Determine the session storage type based on platform relationships.
+    Defaults to 'file' if the relevant environment variable is not set or cannot be decoded.
+    """
     platform_relationships_data = os.environ.get('PLATFORM_RELATIONSHIPS')
-    
+
     if not platform_relationships_data:
         return 'file'
 
     try:
         platform_relationships = json.loads(base64.b64decode(platform_relationships_data))
-        
+
         if 'redis_session' in platform_relationships:
             return 'redis'
-        else:
-            return 'file'
+
+        return 'file'
     except (json.JSONDecodeError, TypeError, ValueError):
         # Catching potential exceptions due to invalid JSON or other issues
         return 'file'
 
 
-def getPlatformEnvironment():
+def get_platform_environment():
+    """
+    Retrieve the type of platform environment where the app is running.
+    Defaults to 'local' if the PLATFORM_ENVIRONMENT_TYPE environment variable is not set.
+    """
     return os.environ.get('PLATFORM_ENVIRONMENT_TYPE', 'local')
