f93b056158a46b62503a272dc873dedc41ab20dc
Tyler Mills <tyler.mills@platform.sh>
2023-11-08 15:58:09 -0500

    Prettify code

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -3,37 +3,44 @@ from flask import Blueprint, jsonify
 import base64
 import json
 
-bp = Blueprint('routes', __name__)
+bp = Blueprint("routes", __name__)
 
-service_relationship ="redis_session"
+service_relationship = "redis_session"
 
-API_PREFIX = '/api/v1'
+API_PREFIX = "/api/v1"
 
-@bp.route(f'{API_PREFIX}/environment')
+
+@bp.route(f"{API_PREFIX}/environment")
 def environment():
-    return jsonify(type=getPlatformEnvironment(), session_storage=getSessionStorageType())
+    return jsonify(
+        type=getPlatformEnvironment(), session_storage=getSessionStorageType()
+    )
 
-@bp.route('/api/')
+
+@bp.route("/api/")
 def home():
     return "Hello from the Python backend!"
 
+
 def getSessionStorageType():
-    platform_relationships_data = os.environ.get('PLATFORM_RELATIONSHIPS')
-    
+    platform_relationships_data = os.environ.get("PLATFORM_RELATIONSHIPS")
+
     if not platform_relationships_data:
-        return 'file'
+        return "file"
 
     try:
-        platform_relationships = json.loads(base64.b64decode(platform_relationships_data))
-        
+        platform_relationships = json.loads(
+            base64.b64decode(platform_relationships_data)
+        )
+
         if service_relationship in platform_relationships:
-            return 'redis'
+            return "redis"
         else:
-            return 'file'
+            return "file"
     except (json.JSONDecodeError, TypeError, ValueError):
         # Catching potential exceptions due to invalid JSON or other issues
-        return 'file'
+        return "file"
 
 
 def getPlatformEnvironment():
-    return os.environ.get('PLATFORM_ENVIRONMENT_TYPE', 'local')
+    return os.environ.get("PLATFORM_ENVIRONMENT_TYPE", "local")
