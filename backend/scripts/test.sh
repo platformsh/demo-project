2afe21953e74ef59c983bcbb94c7a281768361c6
Tyler Mills <tyler.mills@platform.sh>
2025-09-30 15:47:04 -0400

    Troubleshoot pip-audit missing in github workflow

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -3,5 +3,6 @@
 python3 -m venv env
 source env/bin/activate
 uv pip install -r requirements.txt
+uv pip install pip-audit
 # pip-audit will exit 1 if vulnerabilities found (https://github.com/pypa/pip-audit#exit-codes)
-uv pip-audit -r requirements.txt
+uv run pip-audit -r requirements.txt
